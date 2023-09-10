c4b251820e4e8d8faa1780d3070c2f889a3eaa2d
CarlosAngeles19 <carlosangeles.asc@gmail.com>
2023-09-09 23:44:01 -0500

    Agregar archivos de proyecto.

---
+++ lab1pra1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int salona, salonb, salonc, HyM = 0;
+
+        Console.Write("Primer salón en tardar en minutos: ");
+        salona = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Segundo salón en tardar en minutos: ");
+        salonb = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Tercer salón en tardar en minutos: ");
+        salonc = Convert.ToInt32(Console.ReadLine());
+
+        HyM = salona + salonb + salonc;
+
+        Console.WriteLine("El instructor tardará " + HyM + " minutos en revisar los exámenes de todos los salones.");
+    }
+}
